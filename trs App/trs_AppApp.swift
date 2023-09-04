3c26e1b13263b4b4be3d160f40f6dbcce5f4d048
Rae Nadya <rae.nadya511@gmail.com>
2023-09-04 10:00:07 +0800

    Initial Commit

---
+++ trs App/trs_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  trs_AppApp.swift
+//  trs App
+//
+//  Created by Kierae NJ on 4/9/23.
+//
+
+import SwiftUI
+
+@main
+struct trs_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
