adafdc9331704aab19deecfa45e34b75c3be9e4e
Tristan <tristanchay123@gmail.com>
2023-11-17 09:33:15 +0800

    added filtermanager and got filterbuttons to work

---
+++ trs App/Main/Components/Color+Hex.swift
@@ -0,0 +1,18 @@
+//
+//  Color+Hex.swift
+//  trs App
+//
+//  Created by Tristan Chay on 17/11/23.
+//
+
+import SwiftUI
+
+struct Color_Hex: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Color_Hex()
+}
---
+++ trs App/Main/Managers/FilterManager.swift
@@ -0,0 +1,8 @@
+//
+//  FilterManager.swift
+//  trs App
+//
+//  Created by Tristan Chay on 17/11/23.
+//
+
+import Foundation
