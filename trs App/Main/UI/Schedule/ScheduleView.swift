9501c4e26a2ec1d658cd4702298095782fc7bb19
NicoleYu0000 <littleco6688@gmail.com>
2023-11-18 11:25:01 +0800

    add sheet for schedule tab

--- trs App/Main/UI/Schedule/ScheduleView.swift
+++ trs App/Main/UI/Schedule/ScheduleView.swift
@@ -8,12 +8,27 @@
 import SwiftUI
 
 struct ScheduleView: View {
+    
+    @State private var addSheet = false
+    
     var body: some View {
-        NavigationStack {
-            VStack {
-                Text("Schedule View")
+        NavigationStack{
+            VStack{
+                Text("hi")
             }
             .navigationTitle("Schedule")
+            .toolbar{
+                ToolbarItemGroup{
+                    Button{
+                        addSheet = true
+                    }label:{
+                        Image(systemName:"plus")
+                    }
+                }
+            }
+        }
+        .sheet(isPresented: $addSheet){
+            AddSheetView()
         }
     }
 }
