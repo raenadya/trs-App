b435362d8052a3d192a533e6d8ab2f4ae632ac5b
NicoleYu0000 <littleco6688@gmail.com>
2023-11-21 10:42:59 +0800

    individual button for each image in avatar

--- trs App/Main/UI/Settings/AvatarView.swift
+++ trs App/Main/UI/Settings/AvatarView.swift
@@ -29,29 +29,82 @@ struct AvatarView: View {
             HStack(spacing:40){
                 
                 VStack{
-                    Image("bow tie (10c)")
-                        .resizable()
-                    Image("cap (10c)").resizable()
-                    Image("cat ears (30c)").resizable()
-                    Image("chef (20c)").resizable()
-                    Image("crown (20c)").resizable()
-                    Image("devil horns (30c)").resizable()
+                    Button{}label:{
+                        Image("bow tie (10c)")
+                            .resizable()
+                    }
+                    Button{}label:{
+                        Image("cap (10c)")
+                            .resizable()
+                    }
+                    Button{}label:{
+                        Image("cat ears (30c)")
+                            .resizable()
+                    }
+                    Button{}label:{
+                        Image("chef (20c)")
+                            .resizable()
+                    }
+                    Button{}label:{
+                        Image("crown (20c)")
+                            .resizable()
+                    }
+                    Button{}label:{
+                        Image("devil horns (30c)")
+                            .resizable()
+                    }
                 }
                 VStack{
-                    Image("flower crown (10c)").resizable()
-                    Image("graduation hat (20c)").resizable()
-                    Image("headphones (40c)").resizable()
-                    Image("mexican hat (40c)").resizable()
-                    Image("party hat (10c)").resizable()
-                    Image("plant (30c)").resizable()
+                    Button{}label:{
+                        Image("flower crown (10c)")
+                            .resizable()
+                    }
+                    Button{}label:{
+                        Image("graduation hat (20c)")
+                            .resizable()
+                    }
+                    Button{}label:{
+                        Image("headphones (40c)")
+                            .resizable()
+                    }
+                    Button{}label:{
+                        Image("mexican hat (40c)")
+                            .resizable()
+                    }
+                    Button{}label:{
+                        Image("party hat (10c)")
+                            .resizable()
+                    }
+                    Button{}label:{
+                        Image("plant (30c)")
+                            .resizable()
+                    }
                 }
                 VStack{
-                    Image("rabbit ears (30c)").resizable()
-                    Image("santa hat (50c)").resizable()
-                    Image("tiara (50c)").resizable()
-                    Image("top hat (40c)").resizable()
-                    Image("viking helmet (20c)").resizable()
-                    Image("witch hat (20c)").resizable()
+                    Button{}label:{
+                        Image("rabbit ears (30c)")
+                            .resizable()
+                    }
+                    Button{}label:{
+                        Image("santa hat (50c)")
+                            .resizable()
+                    }
+                    Button{}label:{
+                        Image("tiara (50c)")
+                            .resizable()
+                    }
+                    Button{}label:{
+                        Image("top hat (40c)")
+                            .resizable()
+                    }
+                    Button{}label:{
+                        Image("viking helmet (20c)")
+                            .resizable()
+                    }
+                    Button{}label:{
+                        Image("witch hat (20c)")
+                            .resizable()
+                    }
                 }
             }
         }
